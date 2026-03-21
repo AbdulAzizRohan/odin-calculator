852ce48d398733ddcbb22473df297f2ef8c472ca
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2026-03-21 22:38:53 +0600

    added number button and display functionality
    Wrote the `getUserInput()` funciton to get user input when the user
    clicks the number buttons and update the display.

--- script.js
+++ script.js
@@ -34,3 +34,50 @@ function operate(num1, num2, operator) {
       break;
   }
 }
+
+function getUserInput() {
+  const zeroBtn = document.querySelector("#zero");
+  const oneBtn = document.querySelector("#one");
+  const twoBtn = document.querySelector("#two");
+  const threeBtn = document.querySelector("#three");
+  const fourBtn = document.querySelector("#four");
+  const fiveBtn = document.querySelector("#five");
+  const sixBtn = document.querySelector("#six");
+  const sevenBtn = document.querySelector("#seven");
+  const eightBtn = document.querySelector("#eight");
+  const nineBtn = document.querySelector("#nine");
+
+  let numBtnArray = [
+    zeroBtn,
+    oneBtn,
+    twoBtn,
+    threeBtn,
+    fourBtn,
+    fiveBtn,
+    sixBtn,
+    sevenBtn,
+    eightBtn,
+    nineBtn,
+  ];
+
+  const displayValue = document.querySelector("#display");
+
+  const addBtn = document.querySelector("#add");
+  const subtractBtn = document.querySelector("#subtract");
+  const multiplyBtn = document.querySelector("#multiply");
+  const divisionBtn = document.querySelector("#division");
+
+  let operatorBtnArray = [addBtn, subtractBtn, multiplyBtn, divisionBtn];
+
+  let num = +displayValue.textContent;
+
+  numBtnArray.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      num = num * 10 + +btn.textContent;
+      displayValue.textContent = "";
+      displayValue.textContent = num;
+    });
+  });
+}
+
+getUserInput();
